c271d3353c3d5b03d366552b9a0669ea4a5c772d
ktranish <ktranish@outlook.com>
2025-06-03 11:32:05 +0200

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,32 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "jsx-a11y": jsxA11y,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+      // Enable all jsx-a11y rules as errors
+      ...Object.fromEntries(
+        Object.keys(jsxA11y.rules)
+          .filter(
+            (rule) =>
+              rule !== "label-has-for" &&
+              rule !== "accessible-emoji" &&
+              rule !== "label-has-associated-control" &&
+              rule !== "control-has-associated-label",
+          )
+          .map((rule) => [`jsx-a11y/${rule}`, "error"]),
+      ),
+    },
+  },
+  eslintPluginPrettierRecommended,
 ];
 
 export default eslintConfig;
